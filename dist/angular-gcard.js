f4b6ae3a65ec832ebd5f832d7fa6356695e3ef2c
Brian Glick <brian@brian-glick.com>
2013-12-01 00:38:52 -0500

    allow text in links

--- dist/angular-gcard.js
+++ dist/angular-gcard.js
@@ -1,7 +1,7 @@
 // Google+ style cards
 // Project Page: https://github.com/bglick/angular-gcards
 // Styling inspired by https://gist.github.com/eLindemann/5287591
-// Version: 0.1.0
+// Version: 0.1.1
 angular.module('GCard',[]).directive('gCard',function() {
   return {
     restrict:'AE',
@@ -10,6 +10,6 @@ angular.module('GCard',[]).directive('gCard',function() {
     scope:{
       gCard:'='
     },
-    template:'<div class="card" ng-hide="gCard.hidden"><div class="card-title">{{gCard.title}}</div><div ng-transclude class="gcard-body"></div><p class="url" ng-show="gCard.url">{{gCard.url}}</p></div>',
+    template:'<div class="card" ng-hide="gCard.hidden"><div class="card-title">{{gCard.title}}</div><div ng-transclude class="gcard-body"></div><p class="url" ng-show="gCard.link.url"><a ng-href="{{gCard.link.url}}">{{gCard.link.text}}</a></p></div>',
   }
 });
